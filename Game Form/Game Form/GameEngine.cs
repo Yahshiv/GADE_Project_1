b08ecc676eefaf0e0d5f11902b23e46c4f5a4660
Yahshiv Pillay <yahshivpillay817@gmail.com>
2019-10-06 12:39:16 +0200

    Added GameEngine

---
+++ Game Form/Game Form/GameEngine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Form
+{
+    static class GameEngine
+    {
+        public static void SimulateRound(Unit[] units)
+        {
+            Map.round++;
+            for (int i = 0; i < units.Length; i++)
+            {
+                if(Map.remUnits > 1 && units[i].Health > 0)
+                {
+                    units[i].SeekTarget(units);
+                    if(!units[i].IsInRange() || units[i].Health < units[i].MaxHealth * 0.25)
+                    {
+                        units[i].Move();
+                    }
+                    else
+                    {
+                        units[i].Attack(units);
+                    }
+                }
+            }
+        }
+    }
+}
